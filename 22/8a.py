8c745a77c7d9e30b18335fa3bf591302c11485d1
Tom Eastman <tom@eastman.nz>
2022-12-08 18:53:23 +1300

    asdf

---
+++ 22/8a.py
@@ -0,0 +1,80 @@
+from collections import defaultdict, deque
+from dataclasses import dataclass
+from pathlib import PurePath, PurePosixPath
+
+import rich.console
+from aocd.models import Puzzle
+from more_itertools import chunked
+from rich import print
+
+YEAR = 2022
+DAY = 8
+
+console = rich.console.Console()
+puzzle = Puzzle(year=YEAR, day=DAY)
+console.rule(
+    f"{puzzle.title} ({YEAR}-{DAY})"
+)  # ##########################################################
+
+data = puzzle.example_data
+data = puzzle.input_data
+size = len(data.splitlines())
+
+print(data)
+
+
+@dataclass(frozen=True)
+class P:
+    x: int
+    y: int
+
+
+points = dict()
+for l, line in enumerate(data.splitlines()):
+    for c, char in enumerate(line):
+        points[P(c, l)] = int(char)
+
+visible_trees = set()
+
+
+def visible(row):
+    max = -1
+    for p in row:
+        height = points[p]
+        if height > max:
+            visible_trees.add(p)
+            max = height
+
+
+rows = []
+for x in range(size):
+    row = []
+    for y in range(size):
+        row.append(P(x, y))
+    rows.append(row)
+for y in range(size):
+    row = []
+    for x in range(size):
+        row.append(P(x, y))
+    rows.append(row)
+
+for row in rows:
+    visible(row)
+    visible(reversed(row))
+
+print(visible_trees)
+print(len(visible_trees))
+for y in range(size):
+    for x in range(size):
+        p = P(x, y)
+        if p in visible_trees:
+            print(f"[yellow]{points[p]}[/]", end="")
+        else:
+            print(f"[green]{points[p]}[/]", end="")
+    print()
+
+
+print(len(visible_trees))
+console.rule("END")  # ##########################################################
+puzzle.answer_a = len(visible_trees)
+# puzzle.answer_b = cumulative_sizes[candidate]
---
+++ 22/8b.py
@@ -0,0 +1,76 @@
+from collections import defaultdict, deque
+from dataclasses import dataclass
+from pathlib import PurePath, PurePosixPath
+
+import rich.console
+from aocd.models import Puzzle
+from more_itertools import chunked
+from rich import print
+
+YEAR = 2022
+DAY = 8
+
+console = rich.console.Console()
+puzzle = Puzzle(year=YEAR, day=DAY)
+console.rule(
+    f"{puzzle.title} ({YEAR}-{DAY})"
+)  # ##########################################################
+
+# data = puzzle.example_data
+data = puzzle.input_data
+size = len(data.splitlines())
+
+print(data)
+
+
+@dataclass(frozen=True)
+class P:
+    x: int
+    y: int
+
+
+points = dict()
+for l, line in enumerate(data.splitlines()):
+    for c, char in enumerate(line):
+        points[P(c, l)] = int(char)
+
+
+def score(p):
+    up, down, left, right = 0, 0, 0, 0
+    for y in range(p.y - 1, -1, -1):
+        neighbor = points.get(P(p.x, y))
+        if neighbor is None:
+            break
+        up += 1
+        if neighbor >= points[p]:
+            break
+    for y in range(p.y + 1, size):
+        neighbor = points.get(P(p.x, y))
+        if neighbor is None:
+            break
+        down += 1
+        if neighbor >= points[p]:
+            break
+    for x in range(p.x - 1, -1, -1):
+        neighbor = points.get(P(x, p.y))
+        if neighbor is None:
+            break
+        left += 1
+        if neighbor >= points[p]:
+            break
+    for x in range(p.x + 1, size):
+        neighbor = points.get(P(x, p.y))
+        if neighbor is None:
+            break
+        right += 1
+        if neighbor >= points[p]:
+            break
+    return up * down * left * right
+
+
+highest_score = max(score(p) for p in points)
+print(highest_score)
+
+console.rule("END")  # ##########################################################
+# puzzle.answer_a = len(visible_trees)
+puzzle.answer_b = highest_score
